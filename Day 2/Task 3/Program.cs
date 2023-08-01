b5f7cc3cf4556aa41aeec4239b762742bcf0be7f
Abdulsemed Abdelila <abdulsemedabdelila65@gmail.com>
2023-08-01 18:17:57 +0300

    added task 3

---
+++ Day 2/Task 3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class CheckPalindrome
+{
+    public static bool IsPalindrome(string input)
+    {
+        string word = Regex.Replace(input, @"\W+_", "").ToLower();
+        int left = 0;
+        int right = word.Length - 1;
+
+        while (left < right)
+        {
+            if (word[left] != word[right])
+            {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
+
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter a string: ");
+        string input = Console.ReadLine()!;
+
+        bool isPalindrome = CheckPalindrome.IsPalindrome(input);
+
+        if (isPalindrome)
+        {
+            Console.WriteLine($"{input} is a palindrome.");
+        }
+
+        else
+        {
+            Console.WriteLine($"{input} is not a palindrome.");
+        }
+    }
+}
